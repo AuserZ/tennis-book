b3fc6323afbc1af70121b8afdb1dec6eb549d4c9
AuserZ <mzainurroziqinakbar@gmail.com>
2025-12-06 22:46:31 +0700

    add text validation

--- lib/validations.ts
+++ lib/validations.ts
@@ -2,6 +2,7 @@ import { z } from "zod"
 
 export const loginSchema = z.object({
   email: z.string().email("Invalid email address"),
+  password: z.string(),
 })
 
 export const registerSchema = z.object({
